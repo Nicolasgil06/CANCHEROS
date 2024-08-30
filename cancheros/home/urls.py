c743f20561e3abedaa4d497abb4e6988708cb4d4
MiguelProgramador <miguel.comercial08@gmail.com>
2024-08-29 21:49:27 -0500

    modificacion login

--- cancheros/home/urls.py
+++ cancheros/home/urls.py
@@ -1,12 +1,16 @@
 from django.urls import path,include
 from . import views 
+from .views import login_view, logout_view
 
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('reserva/', views.reserva, name='reserva'),
     path('usuario/', views.usuario, name='usuario'),
-    path('login/', views.login, name='login')
+    path('login/', views.login_view, name='login'),
+    path('logout/', logout_view, name='logout'),
+    path('inicio/', views.index, name='inicio')
+    
     
 
 ]
